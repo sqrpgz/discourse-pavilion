948153f04563b309e9daa6bc537ac37b53d1914f
Angus McLeod <angus@mcleod.org.au>
2019-07-25 18:10:57 +1000

    fix the build

--- plugin.rb
+++ plugin.rb
@@ -14,17 +14,34 @@ after_initialize do
       isolate_namespace PavilionHome
     end
   end
-
-  Discourse::Application.routes.append do
+  
+  require_dependency 'lib/homepage_constraint'
+  Discourse::Application.routes.prepend do
+    root to: "pavilion_home/page#index", constraints: HomePageConstraint.new("home")
     get "/home" => "pavilion_home/page#index"
   end
-
-  class PavilionHome::PageController < ::ApplicationController
+  
+  require_dependency 'application_controller'
+  class PavilionHome::PageController < ApplicationController    
     def index
-      render nothing: true
     end
   end
   
   add_to_serializer(:group_user, :bio) { object.user_profile.bio_processed }
   add_to_serializer(:current_user, :homepage_id) { object.user_option.homepage_id }
+  
+  module UserOptionExtension
+    def homepage
+      if homepage_id == 101
+        "home"
+      else
+        super
+      end
+    end
+  end
+  
+  require_dependency 'user_option'
+  class ::UserOption
+    prepend UserOptionExtension
+  end
 end
